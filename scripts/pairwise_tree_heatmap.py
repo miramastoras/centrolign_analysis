dd51cc052cadd583a430ed2e8735245ccdaad9b2
Mira <mastorasmira@gmail.com>
2024-11-22 16:54:54 -0800

    progress with heatmap

--- scripts/pairwise_tree_heatmap.py
+++ scripts/pairwise_tree_heatmap.py
@@ -5,7 +5,8 @@ Purpose: Plot tree with heatmap displaying pairwise input values
 Author: Mira Mastoras, mmastora@ucsc.edu
 python3 pairwise_tree_heatmap.py \
         -t /Users/miramastoras/Desktop/tree_heatmap/KGP4_TRIOS_MAC5_chr12_CPR_EHet30_no_PS_PID_PGT_lifted_over.v1.1_mask.nwk.txt \
-        -s /Users/miramastoras/Desktop/tree_heatmap/samples.txt
+        -s /Users/miramastoras/Desktop/tree_heatmap/samples.txt \
+        -p /Users/miramastoras/Desktop/tree_heatmap/pairwise_combinations.csv
 '''
 
 import argparse
@@ -15,6 +16,8 @@ from Bio import Phylo
 import numpy as np
 from scipy.cluster.hierarchy import dendrogram
 from matplotlib.patches import Rectangle
+import matplotlib.patches as patches
+import csv
 
 def arg_parser():
     '''
@@ -30,6 +33,9 @@ def arg_parser():
     parser.add_argument("-s", "--samples",
                         required=True,
                         help="list of samples to plot from tree")
+    parser.add_argument("-p", "--pairwise_values",
+                        required=True,
+                        help="csv file with pairwise sample combinations in col1-2 and heatmap value in col3")
 
     return parser.parse_args()
 
@@ -43,12 +49,13 @@ def tree_to_linkage_matrix(biopython_tree):
 
     # calculate tree height
     tree_height = max(tree.distance(c) for c in tree.find_clades(terminal=True))
+
     # map labels used in linkage matrix to sample names:
     id_map = {}
     for i, c in enumerate(tree.find_clades(terminal=True)):
         c.comment = (i, 1)
         id_map[i] = c.name
-    print(id_map)
+
     #Phylo.draw(tree)
     # Internal nodes (ancestors) are collected with their distances from the root and sorted in descending order of distance.
     anc_lst = []
@@ -56,7 +63,7 @@ def tree_to_linkage_matrix(biopython_tree):
         d = tree.distance(c)
         anc_lst.append((c, list(c), d))
     anc_lst.sort(key=lambda x: x[2], reverse=True)
-    print(anc_lst)
+
     # running number of node
     nodes = len(list(tree.find_clades(terminal=True)))
     lnk_lst = []
@@ -95,8 +102,18 @@ def main():
     # read back in pruned tree
     tree = Phylo.read("/Users/miramastoras/Desktop/pruned_tree.nwk", "newick")
 
+    pairwise_vals={}
+    # Read in heatmap values
+    with open(args.pairwise_values, "r") as csvfile:
+        reader = csv.reader(csvfile)
+        headers = next(reader)  # Skip the header row
+        for row in reader:
+            key = (row[0], row[1])  # Combine column 1 and 2 as the key
+            value = float(row[2])  # Column 3 as the value
+            pairwise_vals[key] = value
+
     # Plot two matplotlib grids side by side
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6), gridspec_kw = {'wspace':0, 'hspace':0})
+    fig, axes = plt.subplots(1, 3, figsize=(12, 6), gridspec_kw = {'wspace':0, 'hspace':0,'width_ratios': [4, .5,4]})
 
     # convert newick to linkage matrix
     linkage_matrix, id_map = tree_to_linkage_matrix(tree)
@@ -105,32 +122,47 @@ def main():
     dn1 = dendrogram(linkage_matrix, ax=axes[0], orientation='left',labels=[id_map.get(i, str(i)) for i in range(len(id_map))])
 
     axes[0].set_title('Pruned Guide Tree')
-    axes[1].set_title('Pairwise Metric')
+    axes[2].set_title('Pairwise Metric')
 
-    # Remove x and y axis labels
-    axes[1].set_xlabel('')  # Remove x axis label
-    axes[1].set_ylabel('')  # Remove y axis label
-    #axes[1].set_xticks([])  # Remove x ticks
-    axes[1].set_yticks([])  # Remove y ticks
-
+    # get mapping of y position of leaves and leaf labels
     leaf_label_y_map = {}
     for tick in axes[0].get_yticklabels():
-        tick.set_verticalalignment('bottom')
+        tick.set_verticalalignment('bottom') # required for label position to exactly match where leaf ends on dendogram
         leaf_label_y_map[str(tick.get_text())] = int(tick.get_position()[1])
-    # get mapping of y position of leaves and leaf labels
 
+    # set heatmap grid y lim to same y lim as the tree grid, so labels match up. also the label grid in the middle
+    # get y lim of grid 2 and 3 to match the dendogram, so everything lines up with leaves
+    grid1_ylim=[float(axes[0].get_ylim()[0]), float(axes[0].get_ylim()[1])]
+    axes[1].set_ylim(grid1_ylim)
+    axes[2].set_ylim(grid1_ylim)
+    # set x axis in grid 3 symmetrical so diamond is square
+    axes[2].set_xlim(grid1_ylim)
+    axes[1].set_xlim(0,1)
+    axes[1].set_axis_off()
 
-    print(leaf_label_y_map)
-
-    # plot labels
+    # plot sample labels on axes 1
+    label_ends=(float(axes[0].get_ylim()[1])) / 7
     for sample in leaf_label_y_map.keys():
-        plt.plot([0,0.1], [leaf_label_y_map[sample],leaf_label_y_map[sample]], linestyle=':')
+        axes[1].plot([0,1], [leaf_label_y_map[sample],leaf_label_y_map[sample]], linestyle=':')
         axes[1].text(0, leaf_label_y_map[sample], sample, fontsize=6, color='black',va='bottom')
 
-    # set heatmap grid y axes to same scale as the tree grid, so labels match up.
-    axes[1].set_ylim(float(axes[0].get_ylim()[0]), float(axes[0].get_ylim()[1]))
-    axes[1].set_xlim(0,1)
+    # set diamond width to distance between adjacent leaves
+    diamond_diameter=leaf_label_y_map[id_map[1]] - leaf_label_y_map[id_map[0]]
+
+    print(id_map)
+    for pair in pairwise_vals.keys():
+        diamond_xy = [(grid1_ylim[0], leaf_label_y_map[pair[0]]), \
+                    (grid1_ylim[0] - diamond_diameter/2,  leaf_label_y_map[pair[1]] - leaf_label_y_map[pair[0]] ), \
+                    (grid1_ylim[0], leaf_label_y_map[pair[1]]), \
+                    (grid1_ylim[0] + diamond_diameter/2,  leaf_label_y_map[pair[1]] - leaf_label_y_map[pair[0]] )]
+
+        diamond = patches.Polygon(diamond_xy, fill=True, edgecolor='red')
+        axes[2].add_patch(diamond)
+
+    # hide y axis labels for dendogram
+    axes[0].set_yticklabels([])
     # Show the plot
+    axes[2].set_axis_off()
     plt.show()
 if __name__ == '__main__':
     main()
