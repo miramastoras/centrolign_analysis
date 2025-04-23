4397fe78dbcb4ef25959618e4f465df470a78717
Mira <mastorasmira@gmail.com>
2025-04-22 17:20:20 -0700

    revert to original

--- scripts/combine_HOR_flank_dist.py
+++ scripts/combine_HOR_flank_dist.py
@@ -91,7 +91,7 @@ def main():
             # add to dictionary
             mat[(sample1, sample2)] = d
             mat[(sample2, sample1)] = d
-    print(mat[('HG01993.2', 'HG02841.1')])
+    print(mat[("HG01993.2", "HG02841.1")])
     # reorganize as an array
     D = []
 
