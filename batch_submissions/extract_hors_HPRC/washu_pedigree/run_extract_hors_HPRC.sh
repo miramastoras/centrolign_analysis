7072f7aca3a3fd7427377579671222d47b307dcb
Mira <mastorasmira@gmail.com>
2025-09-09 13:48:32 -0700

    extract HORS for washu pedigree

---
+++ batch_submissions/extract_hors_HPRC/washu_pedigree/run_extract_hors_HPRC.sh
@@ -0,0 +1,65 @@
+###############################################################################
+##                             create input jsons                            ##
+###############################################################################
+## workflow name = extract_hors_HPRC
+## experiment = washu_pedigree
+
+## on personal computer...
+
+# Remove top up data from data table
+
+mkdir -p ~/Desktop/github_repos/centrolign_analysis/batch_submissions/extract_hors_HPRC/washu_pedigree/extract_hors_HPRC_input_jsons
+cd ~/Desktop/github_repos/centrolign_analysis/batch_submissions/extract_hors_HPRC/washu_pedigree/extract_hors_HPRC_input_jsons
+
+python3 /Users/miramastoras/Desktop/Paten_lab/hprc_intermediate_assembly/hpc/launch_from_table.py \
+     --data_table ../extract_hors_HPRC_washu_pedigree.csv \
+     --field_mapping ../extract_hors_HPRC_input_mapping.csv \
+     --workflow_name extract_hors_HPRC
+
+## add/commit/push to github (hprc_intermediate_assembly)
+
+###############################################################################
+##                             create launch workflow                      ##
+###############################################################################
+
+## on HPC...
+
+## check that github repo is up to date
+git -C  /private/groups/patenlab/mira/centrolign/github/centrolign_analysis/ pull
+
+# move to working dir
+mkdir -p /private/groups/patenlab/mira/centrolign/batch_submissions/extract_hors_HPRC/washu_pedigree
+cd /private/groups/patenlab/mira/centrolign/batch_submissions/extract_hors_HPRC/washu_pedigree
+
+## get files
+cp -r /private/groups/patenlab/mira/centrolign/github/centrolign_analysis/batch_submissions/extract_hors_HPRC/washu_pedigree/* ./
+
+mkdir -p slurm_logs
+export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
+
+# submit job
+sbatch \
+     --job-name=extract_hors_HPRC \
+     --array=[1-8]%8 \
+     --partition=short \
+     --time=1:00:00 \
+     --cpus-per-task=8 \
+     --exclude=phoenix-[09,10,22,23,24,18] \
+     --mem=60gb \
+     --mail-type=FAIL,END \
+     --mail-user=mmastora@ucsc.edu \
+     ~/progs/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine_debug.sh \
+     --wdl /private/groups/patenlab/mira/centrolign/github/centrolign_analysis/wdl/tasks/extract_hors_HPRC.wdl \
+     --sample_csv extract_hors_HPRC_washu_pedigree.csv \
+     --input_json_path '../extract_hors_HPRC_input_jsons/${SAMPLE_ID}_extract_hors_HPRC.json'
+
+###############################################################################
+##                             write output files                   ##
+###############################################################################
+
+cd /private/groups/patenlab/mira/centrolign/batch_submissions/extract_hors_HPRC/washu_pedigree
+
+python3 /private/groups/hprc/polishing/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+      --input_data_table extract_hors_HPRC_washu_pedigree.csv  \
+      --output_data_table extract_hors_HPRC_washu_pedigree.results.csv  \
+      --json_location '{sample_id}_extract_hors_HPRC_outputs.json'
