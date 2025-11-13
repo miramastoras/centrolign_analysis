b26776b388af2fad796b837fbab4e04b32265a62
Mira <mastorasmira@gmail.com>
2025-11-13 14:08:28 -0800

    submit pairwise consistency

---
+++ analysis_notes/release2_QC_v2/slurm_scripts/pairwise_consistency.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=pairwise_consistency
+#SBATCH --partition=long
+#SBATCH --mail-user=mmastora@ucsc.edu
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --mem=200gb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=logs/centrolign_%x.%j.log
+#SBATCH --time=7-00:00
+#SBATCH --array=[23-26]%5
+
+CSV_FILE=/private/groups/patenlab/mira/centrolign/analysis/pairwise_consistency/centrolign_results.csv
+CHR_SUBGROUP=$(awk "NR==$SLURM_ARRAY_TASK_ID" "$CSV_FILE" | cut -f1)
+CHR=$(awk "NR==$SLURM_ARRAY_TASK_ID" "$CSV_FILE" | cut -f1 | cut -f1 -d"_")
+INDUCED_CIGARS=$(awk "NR==$SLURM_ARRAY_TASK_ID" "$CSV_FILE" | cut -f10)
+
+echo $CSV_FILE
+echo $CHR_SUBGROUP
+echo $CHR
+echo $INDUCED_CIGARS
+
+#time python3 /private/groups/patenlab/mira/centrolign/github/centromere-scripts/benchmarking/pairwise_consistency.py \
+#    ${INDUCED_CIGARS}/pairwise_cigar_ \
+#    /private/groups/patenlab/mira/centrolign/batch_submissions/centrolign/release2_QC_v2/all_pairs/${CHR}/pairwise_cigar/pairwise_cigar_ \
+#    > /private/groups/patenlab/mira/centrolign/analysis/pairwise_consistency/HPRC_r2_QCv2_${CHR_SUBGROUP}_pairwise_consistency.txt
